0438fedf1a8a909e160c5e8aa9fbbe13e99527fc
Hoa JP <itlmh2k3@gmail.com>
2025-09-13 15:10:13 +0700

    set up project: sanity, clerk, middleware

---
+++ app/(store)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-yellow-500">helklo 123</h1>
+      <Button variant='ghost'>Button</Button>
+    </div>
+  );
+}
