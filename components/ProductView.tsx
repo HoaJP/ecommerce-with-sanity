86a7f407d54d623b1942f9ee747c05357e195e76
Hoa JP <itlmh2k3@gmail.com>
2025-09-20 18:08:41 +0700

    build category selector

--- components/ProductView.tsx
+++ components/ProductView.tsx
@@ -1,17 +1,18 @@
 import { Category, Product } from '@/sanity.types'
 import React from 'react'
 import ProductGrid from './ProductGrid'
+import { CategorySelectorComponent } from './ui/category-selector'
 
 interface ProductViewProps{
     products: Product[],
     categories: Category[]
 }
-const ProductView = ({products}: ProductViewProps) => {
+const ProductView = ({products, categories}: ProductViewProps) => {
   return (
     <div className='flex flex-col'>
         {/* categories */}
         <div className="w-full sm:w-[200px]">
-            {/* <CategorySelectorComponent categories={categories}/> */}
+            <CategorySelectorComponent categories={categories}/>
         </div>
         {/* products */}
         <div className="flex-1">
